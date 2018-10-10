64f5df6f57d2cc42fea2724f298eba56e21047e5
kamijin_fanta <kamijin@live.jp>
2018-10-10 12:26:01 +0900

    C#でもDataStore取得サンプルを追加

---
+++ csharp/datastore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace datastore
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.Write("トークンを入力 > ");
+            var token = Console.ReadLine();
+            var url = "https://api.sakura.io/datastore/v1/messages?token=" + token;
+            var result = await client.GetStringAsync(url);
+            Console.WriteLine(result);
+        }
+    }
+}
